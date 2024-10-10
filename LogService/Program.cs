6f52a39ebc6e2cd8bccc87b3703a4bbdf4caba06
Penah_vk38 <Penah_vk38@STHQ0126-03>
2024-10-10 17:09:48 +0400

    Add project files.

---
+++ LogService/Program.cs
@@ -0,0 +1,33 @@
+using LogService.Services.Abstract;
+using LogService.Services.Concrete;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.WebHost.UseUrls("https://*:10787");
+
+builder.Services.AddScoped<ILogInformationService, LogInformationService>();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
